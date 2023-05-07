67a8f74c4b07c686662c4ab4c6d2ece99c5c26b8
YuriSerhiienko <126407379+YuriSerhiienko@users.noreply.github.com>
2023-05-07 14:20:09 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,98 @@
+from classes import Record, AddressBook, Phone
+
+phonebook = AddressBook()
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except KeyError:
+            return "There is no such name"
+        except ValueError:
+            return "Give me name and phone please"
+        except IndexError:
+            return "Enter user name"
+        except TypeError:
+            return "Incorrect values"
+    return inner
+
+@input_error
+def greeting():
+    return "How can I help you?"
+
+def unknown_command():
+    return "Unknown command"
+
+@input_error
+def exit():
+    return None
+
+@input_error
+def add_user(name, phone):
+    record = Record(name, phone)
+    phonebook.add_record(record)
+    return "Contact added successfully"
+
+@input_error
+def change_phone(name, phone):
+    records = phonebook.search(name=name)
+    if records:
+        records[0].phones = [Phone(phone)]
+        return "Contact updated successfully."
+    else:
+        return "There is no such name"
+
+def show_all():
+    if not phonebook:
+        return "The phonebook is empty"
+    result = ''
+    for record in phonebook.values():
+        result += f"{record.name.value}: {record.phones[0].value}\n"
+    return result.rstrip()
+
+@input_error
+def get_phone_number(name):
+    records = phonebook.search(name=name)
+    if records:
+        return records[0].phones[0].value
+    else:
+        return "There is no such name"
+
+commands = {
+    'hello': greeting,
+    'add': add_user,
+    'change': change_phone,
+    'show all': show_all,
+    "phone": get_phone_number,
+    'exit': exit,
+    'good bye': exit,
+    'close': exit,
+}
+
+def main():
+    while True:
+        command, *args = input(">>> ").strip().split(' ', 1)
+        if commands.get(command):
+            handler = commands.get(command)
+            if args:
+                args = args[0].split()
+                result = handler(*args)
+            else:
+                result = handler(*args)
+        elif args and commands.get(command + ' ' + args[0]):
+            command = command + ' ' + args[0]
+            args = args[1:]
+            handler = commands.get(command)
+            result = handler(*args)
+        else:
+            result = unknown_command()
+
+        if not result:
+            print('Good bye!')
+            break
+
+        print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ classes.py
@@ -0,0 +1,43 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Record:
+    def __init__(self, name, phone=None):
+        self.name = Name(name)
+        self.phones = []
+        if phone:
+            self.add_phone(phone)
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+    
+    def edit_phone(self, index, phone):
+        self.phones[index] = Phone(phone)
+    
+    def delete_phone(self, index):
+        del self.phones[index]
+    
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def search(self, name=None, phone=None):
+        result = []
+        for record in self.data.values():
+            if name and record.name.value != name:
+                continue
+            if phone and not any(p.value == phone for p in record.phones):
+                continue
+            result.append(record)
+        return result
