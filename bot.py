0079c21f80767f26c082535b8054d2441b8c84a8
YuriSerhiienko <126407379+YuriSerhiienko@users.noreply.github.com>
2023-05-07 22:00:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,6 @@ def add_user(name, phone=None):
         phonebook.add_record(record)
         return "Contact added successfully"
 
-
 @input_error
 def change_phone(name, phone):
     record = phonebook.get(name, None)
@@ -64,20 +63,38 @@ def show_all():
         result += f"{record.name.value}: {phones}\n"
     return result.rstrip()
 
-
 @input_error
 def get_phone_number(name):
-    record = phonebook.get(name, None)
-    if record:
-        if record.phones:
-            return ', '.join([phone.value for phone in record.phones])
-        else:
-            return "Contact has no data"
+    result = phonebook.search(name=name)
+    if result:
+        result_strings = [f"{contact.name.value}: {', '.join([phone.value for phone in contact.phones])}" for contact in result for phone in contact.phones]
+        return ", ".join(result_strings)
     else:
         return "There is no such name"
-
-
-
+    
+def get_name(phone):
+    result = phonebook.search(phone=phone)
+    if result:
+        result_strings = [f"{contact.name.value}: {phone.value}" for contact in result for phone in contact.phones]
+        return ", ".join(result_strings)
+    else:
+        return "No records found for that phone number"
+    
+def search_by_criteria(criteria):
+    if criteria and (criteria.isdigit() or "@" in criteria):
+        result = phonebook.search(phone=criteria)
+        result_strings = [f"{contact.name.value}: {', '.join([phone.value for phone in contact.phones])}" for contact in result]
+        if result_strings:
+            return "\n".join(result_strings)
+        else:
+            return "No records found for that criteria"
+    else:
+        result = phonebook.search(name=criteria)
+        result_strings = [f"{contact.name.value}: {', '.join([phone.value for phone in contact.phones])}" for contact in result]
+        if result_strings:
+            return "\n".join(result_strings)
+        else:
+            return "No records found for that criteria"
 
 commands = {
     'hello': greeting,
@@ -85,9 +102,11 @@ commands = {
     'change': change_phone,
     'show all': show_all,
     "phone": get_phone_number,
+    'name': get_name,
     'exit': exit,
     'good bye': exit,
     'close': exit,
+    'search': search_by_criteria,
 }
 
 def main():
